3e8ea637cd28595f8714f00888e553065e51b798
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-15 15:47:09 +0200

    add Index In ComicModelController & Layouts + Partials Commit

--- app/Http/Controllers/ComicModelController.php
+++ app/Http/Controllers/ComicModelController.php
@@ -12,7 +12,7 @@ class ComicModelController extends Controller
      */
     public function index()
     {
-        //
+        return view('comics.index');
     }
 
     /**
---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>COMICS</h1>
+    <a href="{{ route('home') }}"> HOME </a>
+@endsection
